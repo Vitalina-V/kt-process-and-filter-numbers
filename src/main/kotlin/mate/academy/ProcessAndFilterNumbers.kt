b38df9b4f75553b4902daecb53a3e743d3cdb5d7
Volkivska <vitalina.v@air-dr.com>
2025-08-11 10:26:56 +0300

    processAndFilterNumbers impl

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
+const val VALUE_LIMIT = 25
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    return numbers.map {
+        if (it.rem(2) != 0) it.times(2) else it.div(2)
+    }.filter { it > VALUE_LIMIT }
 }
